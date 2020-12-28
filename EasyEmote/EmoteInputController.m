4bb10081cb3718dff94ef01148920d4c089e065e
Jacky He <jacky.he.juncheng@gmail.com>
2020-12-27 20:38:10 -0500

    Revert "modified info.plist"
    This reverts commit d0be37ab863a20d08a54ffa6d8aec30b94487c7b.

--- EasyEmote/EmoteInputController.m
+++ EasyEmote/EmoteInputController.m
@@ -34,14 +34,12 @@
     _currentClient = sender;
     extern Preferences* preferences;
 //    [preferences train_model];
-    NSLog(@"DEBUGMESSAGE: Server activated :)");
 }
 
 -(void)deactivateServer:(id)sender
 {
     extern IMKCandidates* candidates;
     [candidates hide];
-    NSLog(@"DEBUGMESSAGE: Server deactivated :(");
 }
 
 -(void)commitComposition:(id)sender
